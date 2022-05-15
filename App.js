60d579e9c8b99a743a6733ae81049ab029da251d
adrianagh13 <gomezhinojosa.adriana@gmail.com>
2022-05-15 18:21:40 -0500

    -Home screen created

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from "react-redux";
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build uber</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <SafeAreaView />
+      <Text>Let's build HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+
+});
